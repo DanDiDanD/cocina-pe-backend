b83eaca16c9bba77dd6cd02b904c50add2a8670e
Daniel Angeles Rojas <Angeles-Daniel@users.noreply.github.com>
2021-03-25 21:05:33 -0500

    cambio env

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ const app = express();
 
 // Configure Header HTTP
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    next();
-  });
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 
   
   app.use(helmet());
